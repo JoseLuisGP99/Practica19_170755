514664b62f5a3329a100be16b4bf4da137e55573
JoseLuisGP99 <joselgp_1999@hotmail.com>
2023-12-01 14:13:06 -0600

    first commit

---
+++ src/seed/players.js
@@ -0,0 +1,91 @@
+const players = [
+    {
+        name: "Joz",
+        email: "joz@outlook.com",
+        password: "12345",
+        nickname: "joze",
+        birthdate: "2002-05-05",
+        portrait_img: "uwu.png"
+    },
+    {
+        name: "EPedro",
+        email: "pedro@outlook.com",
+        password: "12345",
+        nickname: "pepe",
+        birthdate: "2001-04-03",
+        portrait_img: "left.png"
+    },
+    {
+        name: "Joel",
+        email: "joel@gmail.com",
+        password: "password123",
+        nickname: "jojo",
+        birthdate: "1995-12-15",
+        portrait_img: "john.png"
+    },
+    {
+        name: "Areli",
+        email: "areli@yahoo.com",
+        password: "securepass",
+        nickname: "arich",
+        birthdate: "1990-08-22",
+        portrait_img: "ari.jpg"
+    },
+    {
+        name: "Beto",
+        email: "beto@hotmail.com",
+        password: "bob123",
+        nickname: "betich",
+        birthdate: "1988-03-10",
+        portrait_img: "beto.png"
+    },
+    {
+        name: "Sandra",
+        email: "sandra@gmail.com",
+        password: "12345",
+        nickname: "sandrix",
+        birthdate: "1993-06-27",
+        portrait_img: "sandri.png"
+    },
+    {
+        name: "Daniela",
+        email: "daniela@yahoo.com",
+        password: "daniela456",
+        nickname: "danman",
+        birthdate: "1987-11-18",
+        portrait_img: "daniel.jpg"
+    },
+    {
+        name: "Sofia",
+        email: "sofia@hotmail.com",
+        password: "sophie789",
+        nickname: "sofis",
+        birthdate: "1998-09-03",
+        portrait_img: "sophie.png"
+    },
+    {
+        name: "Mari",
+        email: "Mari@gmail.com",
+        password: "1234",
+        nickname: "marihuas",
+        birthdate: "1985-02-14",
+        portrait_img: "mari.jpg"
+    },
+    {
+        name: "Lulu",
+        email: "lulu@yahoo.com",
+        password: "1234",
+        nickname: "lulu",
+        birthdate: "1996-07-08",
+        portrait_img: "lulu.png"
+    },
+    {
+        name: "Alfredo",
+        email: "alfredo@gmail.com",
+        password: "13456",
+        nickname: "alfredito",
+        birthdate: "1994-10-20",
+        portrait_img: "alfred.png"
+    }
+]
+export default players
